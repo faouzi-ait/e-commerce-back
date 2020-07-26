a1e5007a3344ae352e326cffd678972b555a2a6e
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2020-07-26 21:32:06 +0200

    Updated mail config

--- configuration/oauth.js
+++ configuration/oauth.js
@@ -1,7 +1,8 @@
 module.exports = {
-    type: 'oauth2',
-    user: process.env.user,
-    clientId: process.env.clientId,
-    clientSecret: process.env.clientSecret,
-    refreshToken: '1//0473R-aoau6JSCgYIARAAGAQSNwF-L9IrWQn90L6gAY_pyWEg7OX2VNI7kX3rqAA4APmZPH1I1rh7kx7bNn3H-6Kuqqu0dnmEKx0'
-}
+  type: "oauth2",
+  user: process.env.user,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  refreshToken:
+    "1//04xcRjqkG35BoCgYIARAAGAQSNwF-L9IrG0TduXXxipgUvyrGtz6b_uytKtpTcGFHmyeffS7gz9e7PEu1YeIGSl7FNeT_so58FcA",
+};
